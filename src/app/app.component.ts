c1651c9a1dcf0f63b020382725db2abd3dc3e211
Md.Al-amin <al.amin@tigerit.com>
2022-10-28 09:21:31 +0600

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { ElectronService } from './core/services';
+import { TranslateService } from '@ngx-translate/core';
+import { APP_CONFIG } from '../environments/environment';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav'
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements  AfterViewInit {
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+
+  constructor(
+    private electronService: ElectronService,
+    private translate: TranslateService,
+    private observer: BreakpointObserver
+  ) {
+    this.translate.setDefaultLang('en');
+    console.log('APP_CONFIG', APP_CONFIG);
+
+    if (electronService.isElectron) {
+      console.log(process.env);
+      console.log('Run in electron');
+      console.log('Electron ipcRenderer', this.electronService.ipcRenderer);
+      console.log('NodeJS childProcess', this.electronService.childProcess);
+    } else {
+      console.log('Run in browser');
+    }
+  }
+
+  ngAfterViewInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
+  }
+}
